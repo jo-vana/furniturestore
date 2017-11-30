c48583e703a664152095a01002cfb2bab28ff45e
jo-vana <jovananukic85@gmail.com>
2017-11-30 14:33:23 +0100

    changes for furniture node page

--- themes/custom/furniture_theme/scripts/app/app.js
+++ themes/custom/furniture_theme/scripts/app/app.js
@@ -2,4 +2,26 @@
 
 	'use strict';
 
+    // Drupal.behaviors.furniture_node_page = {
+    //
+    	// attach: $(document).ready(function(){
+    //
+     //        $('ul.tabs li').click(function(){
+     //            var tab_id = $(this).attr('data-tab');
+    //
+     //            $('ul.tabs li').removeClass('current');
+     //            $('.tab-content').removeClass('current');
+    //
+     //            $(this).addClass('current');
+     //            $("#"+tab_id).addClass('current');
+     //        });
+    //
+     //    })
+	// };
+
+
 })(jQuery);
+
+
+
+
