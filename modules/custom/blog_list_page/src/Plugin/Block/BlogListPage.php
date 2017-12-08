60621ccfa4ea9a4d3379ca58db9dc28f1662ad8b
vanja <jovana.nukic@canicinteractive.com>
2017-12-08 13:59:45 +0100

    FS-BE-26 -- Blog List Page.

--- modules/custom/blog_list_page/src/Plugin/Block/BlogListPage.php
+++ modules/custom/blog_list_page/src/Plugin/Block/BlogListPage.php
@@ -17,10 +17,11 @@ use Drupal\Core\Block\BlockBase;
  *   category = @Translation("Blocks")
  * )
  */
-class BlogListPage extends BlockBase implements BlockPluginInterface{
-        /**
-         * {@inheritdoc}
-         */
+class BlogListPage extends BlockBase implements BlockPluginInterface
+{
+    /**
+     * {@inheritdoc}
+     */
     function buildContent()
     {
         $query = \Drupal::database()->select('node_field_data', 'n');
@@ -28,13 +29,13 @@ class BlogListPage extends BlockBase implements BlockPluginInterface{
 
         $query->innerJoin('node__field_blog_image', 'bi', 'bi.entity_id = n.nid');
 
-        $query->innerJoin('node__field_date_blog', 'fdb', 'fdb.entity_id = n.nid');
-
-        $query->innerJoin('node__body', 'b', 'b.entity_id = n.nid');
-
         $query->innerJoin('node__field_categories', 'fc', 'fc.entity_id = n.nid');
 
         $query->innerJoin('taxonomy_term_field_data', 't', 'fc.field_categories_target_id = t.tid');
+
+        $query->innerJoin('node__field_date_blog', 'fdb', 'fdb.entity_id = n.nid');
+
+        $query->innerJoin('node__body', 'b', 'b.entity_id = n.nid');
 
         $query->innerJoin('node__field_blog_author', 'ba', 'ba.entity_id = n.nid');
 
@@ -45,55 +46,54 @@ class BlogListPage extends BlockBase implements BlockPluginInterface{
         $query->addField('u', 'uid');
         $query->addField('n', 'title');
         $query->addField('b', 'body_value');
-        $query->addField('bi', 'field_blog_image_target_id', 'image');
         $query->addField('t', 'name', 'taxonomy_name');
+        $query->addField('bi', 'field_blog_image_target_id', 'image');
         $query->addField('fdb', 'field_date_blog_value');
         $query->addField('u', 'name', 'username');
 
-        $data = [];
+        $results = $query->execute()->fetchAll(\PDO::FETCH_GROUP);
 
-        $results = $query->execute()->fetchAll();
+        $output = [];
 
-        // \PDO::FETCH_GROUP
+        foreach($results as $key => $result ) {
+            $entry = [];
+            foreach( $result as $node ) {
+                $file = File::load($node->image);
+                $url = \Drupal\image\Entity\ImageStyle::load('blog_default_img')->buildUrl($file->getFileUri());
+                $alias = \Drupal::service('path.alias_manager')->getAliasByPath('/node/' . $key);
+                $alias2 = \Drupal::service('path.alias_manager')->getAliasByPath('/' . $node->taxonomy_name);
+                $alias3 = \Drupal::service('path.alias_manager')->getAliasByPath('/user/' . $node->uid);
 
-        foreach ( $results as $result ) {
-            $file = File::load($result->image);
-            $url = \Drupal\image\Entity\ImageStyle::load('blog_default_img')->buildUrl($file->getFileUri());
-            $alias = \Drupal::service('path.alias_manager')->getAliasByPath('/node/'.$result->nid);
-            $alias2 = \Drupal::service('path.alias_manager')->getAliasByPath('/'.$result->taxonomy_name);
-            $alias3 = \Drupal::service('path.alias_manager')->getAliasByPath('/user/'.$result->uid);
+                $alias_tax = str_replace(' ', '-', $alias2);
 
-            $alias_tax = str_replace(' ', '-', $alias2);
-
-            $data[] = [
-                'alias' => $alias,
-                'alias2' => $alias_tax,
-                'alias3' => $alias3,
-                'title' => $result->title,
-                'body' =>  substr(strip_tags(str_replace(array("\r", "\n"), '', $result->body_value)), 0, 140),
-                'taxonomy_name' =>  [
-                    'name'  => strip_tags($result->taxonomy_name),
-                    'url'   => $alias2
-                ],
-                'image' => $url,
-                'field_date_blog_value' => date('F d, Y', $result->field_date_blog_value),
-                'username' => $result->username,
-            ];
-
+                $entry['nid'] = $alias;
+                $entry['tid'] = $alias_tax;
+                $entry['uid'] = $alias3;
+                $entry['title'] = $node->title;
+                $entry['body'] = substr(strip_tags(str_replace(array("\r", "\n"), '', $node->body_value)), 0, 110);
+                $entry['taxonomy_name'][] =[
+                    'name'  => strip_tags($node->taxonomy_name),
+                    'url'   => $alias_tax
+                ];
+                $entry['image'] = $url;
+                $entry['field_date_blog_value'] = date('F d, Y', $node->field_date_blog_value);
+                $entry['username'] = $node->username;
+            }
+            $output[] = $entry;
         }
-
-        return $data;
+        return $output;
     }
 
     /**
      * {@inheritdoc}
      */
-    public function build () {
+    public function build()
+    {
 
         return array(
-            '#theme'    => 'blog_list_page',
-            '#content'  => $this->buildContent(),
-            '#cache'    => [
+            '#theme' => 'blog_list_page',
+            '#content' => $this->buildContent(),
+            '#cache' => [
                 'max-age' => 0,
             ],
         );
