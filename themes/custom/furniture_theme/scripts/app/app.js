013c3fb5b98d10c2629795f34a8b8b7a91c83e8a
jo-vana <jovananukic85@gmail.com>
2017-12-10 01:49:34 +0100

    FS-FE-14- Creating new main navigation for mobile-in progress

--- themes/custom/furniture_theme/scripts/app/app.js
+++ themes/custom/furniture_theme/scripts/app/app.js
@@ -19,6 +19,45 @@
         }
     };
 
+    // Function for adding new class on li>ul if there is another ul inside
+
+    Drupal.behaviors.newClassForMenu = {
+        attach:function(context) {
+            $('li.nav-list-1', context).has('ul').parent('ul').removeClass('nav-list-1').addClass('has-children');
+
+        }
+
+    };
+
+    // Function for mobile version for main navigation
+
+    Drupal.behaviors.mobileMenu = {
+      attach:function(context, settings) {
+
+          var link = $('#block-mainnavigationmobile ul.navbar-nav li').find('a');
+
+                  // If a link has a dropdown, add sub menu toggle.
+                  $('#block-mainnavigationmobile ul.navbar-nav li a').click(function(e) {
+                      // e.preventDefault();
+
+                      $(this).siblings('.dropdown-menu').toggle();
+                      // Close one dropdown when selecting another
+                      $('.dropdown-menu').not($(this).siblings()).hide();
+                      e.stopPropagation();
+                  });
+                  // Clicking away from dropdown will remove the dropdown class
+                  $('html').click(function() {
+                      $('.dropdown-menu').hide();
+                  });
+                  // Toggle open and close nav styles on click
+                  $('#nav-toggle').click(function() {
+                      $('#block-mainnavigationmobile ul').toggle();
+                  });
+
+      }
+
+    };
+
 })(jQuery);
 
 
