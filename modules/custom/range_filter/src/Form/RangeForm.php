ed21e5023502567ef936c9af42007c3747484b51
vanja <jovana.nukic@canicinteractive.com>
2017-12-27 16:24:25 +0100

    Range Filter

--- modules/custom/range_filter/src/Form/RangeForm.php
+++ modules/custom/range_filter/src/Form/RangeForm.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Drupal\furniture_list_page\Form;
+namespace Drupal\range_filter\Form;
 
 use Drupal\Core\Database\Database;
 use Drupal\file\Entity\File;
@@ -9,16 +9,16 @@ use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
 
 /**
- * Class SearchForm.
+ * Class RangeForm.
  */
-class SearchForm extends FormBase {
+class RangeForm extends FormBase {
 
 
 	/**
 	 * {@inheritdoc}
 	 */
 	public function getFormId () {
-		return 'furniture_list_form';
+		return 'range_form';
 	}
 
 	/**
@@ -30,42 +30,20 @@ class SearchForm extends FormBase {
 
 		$form[ '#method' ] = 'GET';
 
-		$form[ 'filters' ] = [
-			'#type'        => 'fieldset',
-			'#title'       => t( 'filters' ),
-			'#collapsible' => true,
-			'#attributes'  => array( 'class' => array( 'inline' ) ),
-		];
+		$form ['filters']['min_price'] = array(
+			'#type' => 'number',
+			'#placeholder' => t('Min'),
+			'#min' => 0,
+			'#max' => 1000000,
+			'#default_value' => isset( $_GET[ 'min_price' ] ) ? $_GET[ 'min_price' ] : '',
+		);
 
-		$options = [
-			0 => 'Default sorting',
-			1 => 'Sort by popularity',
-			2 => 'Sort by average rating',
-			3 => 'Sort by newness',
-			4 => 'Sort by price low to high',
-			5 => 'Sort by price high to low',
-		];
-
-		$form[ 'filters' ][ 'sort' ] = [
-			'#type'          => 'select',
-			'#options'       => $options,
-			'#default_value' => isset( $_GET[ 'sort' ] ) ? $_GET[ 'sort' ] : '',
-		];
-
-		$form[ 'filters' ][ 'actions' ][ '#type' ] = 'actions';
-		$form[ 'filters' ][ 'actions' ][ 'submit' ] = [
-			'#type'  => 'submit',
-			'#value' => $this->t( 'Submit' ),
-		];
-
-		$data = $this->buildContent();
-
-		$form[ 'content' ][ 'data' ] = $data;
-
-		$form[ '#theme' ] = 'furniture_list_page';
-
-		$form[ 'pager' ] = array(
-			'#type' => 'pager',
+		$form ['filters']['max_price'] = array(
+			'#type' => 'number',
+			'#placeholder' => t('Max'),
+			'#min' => 0,
+			'#max' => 1000000,
+			'#default_value' => isset( $_GET[ 'max_price' ] ) ? $_GET[ 'max_price' ] : '',
 		);
 
 		return $form;
@@ -74,85 +52,32 @@ class SearchForm extends FormBase {
 	private function buildContent () {
 
 		$query = \Drupal::database()->select( 'node_field_data', 'n' );
-		$query->condition( 'n.type', 'furniture', '=' );
+		$query->innerJoin('node__field_price', 'fp', 'n.nid = fp.entity_id');
 
-		$query->innerJoin('node__field_price', 'fp', 'fp.entity_id = n.nid' );
+		$min_price = $_GET[ 'min_price' ];
 
-		$query->innerJoin('node__field_fur_image', 'fi', 'fi.entity_id = n.nid');
-
-		$query->condition('fi.delta', 0, '=');
-
-		$query->innerJoin('node__field_categories', 'fc', 'fc.entity_id = n.nid' );
-
-		$query->innerJoin('taxonomy_term_field_data', 't', 'fc.field_categories_target_id = t.tid' );
-
-		$query->innerJoin('node_counter', 'nc', 'nc.nid = n.nid');
-
-		if ( !empty( $_GET[ 'title' ] ) ) {
-			$query->condition( 'title', $_GET[ 'title' ], 'LIKE' );
+		if ( isset( $min_price ) ) {
+			$query->condition( 'fp.field_price_value', $min_price, '>=' );
 		}
 
-		$query->addField('n', 'nid');
-		$query->addField('n', 'status');
-		$query->addField('n', 'title');
-		$query->addField('t', 'tid');
-		$query->addField('t', 'name', 'taxonomy_name');
-		$query->addField('fi', 'field_fur_image_target_id', 'image');
-		$query->addField('fp', 'field_price_value', 'price');
-
-		$sort = 0;
-		if ( isset( $_GET[ 'sort' ] ) ) {
-			$sort = $_GET[ 'sort' ];
+		$max_price = $_GET['max_price'];
+		if (!empty($max_price)) {
+			if (isset($max_price)) {
+				$query->condition('fp.field_price_value', $max_price, '<=');
+			}
 		}
 
-		if ( $sort == 0 ) {
-			$query->orderBy( 'nid', 'ASC');
-		} else if ( $sort == 1 ) {
-			$query->orderBy( 'totalcount', 'DESC' );
-		} else if ( $sort == 2 ) {
-			$query->innerJoin('node__field_reviews', 'fr', 'fr.entity_id = n.nid');
-			$query->innerJoin('comment_entity_statistics', 'ces', 'fr.entity_id = ces.entity_id');
-			$query->innerJoin('comment_field_data', 'cfd', 'ces.entity_id = cfd.entity_id');
-			$query->innerJoin('comment__field_your_rating', 'cff', 'cfd.cid = cff.entity_id');
-			$query->orderBy( 'field_your_rating_rating', 'DESC' );
-		} else if ( $sort == 3 ) {
-			$query->orderBy( 'nid', 'DESC' );
-		} else if ( $sort == 4 ) {
-			$query->orderBy( 'price', 'ASC' );
-		} else if ( $sort == 5) {
-			$query->orderBy( 'price', 'DESC' );
-		}
+	}
 
-		$data = [];
+	public function build () {
 
-		$results = $query->execute()->fetchAll(\PDO::FETCH_GROUP);
-
-		foreach($results as $key => $result ) {
-			$entry = [];
-			foreach( $result as $node ) {
-				$file = File::load($node->image);
-				$url = \Drupal\image\Entity\ImageStyle::load('furniture_teaser_img')->buildUrl($file->getFileUri());
-				$alias = \Drupal::service('path.alias_manager')->getAliasByPath('/node/' . $key);
-				$alias2 = \Drupal::service('path.alias_manager')->getAliasByPath('/' . $node->taxonomy_name);
-
-				$alias_tax = str_replace(' ', '-', $alias2);
-
-				$entry['nid'] = $alias;
-				$entry['tid'] = $alias_tax;
-				$entry['title'] = $node->title;
-				$entry['price'] = $node->price;
-				$entry['taxonomy_name'][] =[
-					'name'  => strip_tags($node->taxonomy_name),
-					'url'   => $alias_tax
-				];
-				$entry['image'] = $url;
- 			}
-			$data[] = $entry;
-		}
-
-		$data['counters'] = count($data);
-
-		return $data;
+		return array(
+			'#theme'    => 'range_filter',
+			'#content'  => $this->buildContent(),
+			'#cache'    => [
+				'max-age' => 0,
+			],
+		);
 	}
 
 	/**
