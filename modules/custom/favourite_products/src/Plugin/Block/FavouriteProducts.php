2c33ba16e7e0772688388983452e109edf8f55c6
vanja <jovana.nukic@canicinteractive.com>
2018-01-09 09:52:28 +0100

    Added new fields for hover.

--- modules/custom/favourite_products/src/Plugin/Block/FavouriteProducts.php
+++ modules/custom/favourite_products/src/Plugin/Block/FavouriteProducts.php
@@ -26,11 +26,17 @@ class FavouriteProducts extends BlockBase implements BlockPluginInterface{
         $query = \Drupal::database()->select('node_field_data', 'n');
         $query->condition('n.type', 'furniture', '=');
 
+        $query->innerJoin('node__body', 'b', 'b.entity_id = n.nid');
+
         $query->innerJoin('node__field_fur_image', 'fi', 'fi.entity_id = n.nid');
 
         $query->condition('fi.delta', 0, '=');
 
         $query->innerJoin('node__field_price', 'fp', 'fp.entity_id = n.nid' );
+
+        $query->innerJoin('node__field_categories', 'fc', 'fc.entity_id = n.nid' );
+
+        $query->innerJoin('taxonomy_term_field_data', 't', 'fc.field_categories_target_id = t.tid' );
 
         $query->innerJoin('node_counter', 'nc', 'nc.nid = n.nid');
 
@@ -40,21 +46,48 @@ class FavouriteProducts extends BlockBase implements BlockPluginInterface{
         $query->addField('n', 'title');
         $query->addField('fi', 'field_fur_image_target_id', 'image');
         $query->addField('fp', 'field_price_value');
+        $query->addField('b', 'body_value', 'body');
+        $query->addField('t', 'name', 'taxonomy_name');
+        $query->addField('t', 'tid');
 
         $data = [];
 
-        $results = $query->execute()->fetchAll();
+        $results = $query->execute()->fetchAll(\PDO::FETCH_GROUP);
 
-        foreach ( $results as $result ) {
-            $file = File::load($result->image);
-            $url = \Drupal\image\Entity\ImageStyle::load('furniture_fav_321x321')->buildUrl($file->getFileUri());
-            $alias_node = \Drupal::service('path.alias_manager')->getAliasByPath('/node/'.$result->nid);
-            $data[] = [
-                'alias' => $alias_node,
-                'title' => $result->title,
-                'field_price_value' => $result->field_price_value,
-                'image' => $url,
-            ];
+        foreach($results as $key => $result ) {
+            $entry = [];
+            foreach( $result as $node ) {
+                $file = File::load($node->image);
+                $url = \Drupal\image\Entity\ImageStyle::load('furniture_default_img')->buildUrl($file->getFileUri());
+                $alias_node = \Drupal::service('path.alias_manager')->getAliasByPath('/node/' . $key);
+                $alias_taxonomy = \Drupal::service('path.alias_manager')->getAliasByPath('/' . $node->taxonomy_name);
+
+                $alias_tax = str_replace(' ', '-', $alias_taxonomy);
+
+                $entry['nid'] = $alias_node;
+                $entry['tid'] = $alias_tax;
+                $entry['title'] = $node->title;
+                $entry['field_price_value'] = $node->field_price_value;
+                $entry['body'] = substr(strip_tags(str_replace(array("\r", "\n"), '', $node->body)), 0, 400);
+                $entry['image'] = $url;
+
+                //@todo If current logic that node has maximum input values of 2 taxonomy terms is changed, change logic bellow.
+                if (!isset($entry['taxonomy_name'][0])) {
+                    $entry['taxonomy_name'][] = [
+                        'name' => strip_tags($node->taxonomy_name),
+                        'url' => 'furniture/?taxonomy=' . $node->taxonomy_name,
+                    ];
+                } else {
+                    if ($entry['taxonomy_name'][0]['name'] !== $node->taxonomy_name) {
+                        $entry['taxonomy_name'][1] = [
+                            'name' => strip_tags($node->taxonomy_name),
+                            'url' => 'furniture/?taxonomy=' . $node->taxonomy_name,
+                        ];
+                    }
+                }
+            }
+            $data[] = $entry;
+
             if (count($data) > 8){
                 if( !empty($data[7])) {
                     unset($data[7]);
